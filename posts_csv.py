cb07790937f3de1292c0346370824a242be54cf6
Giorgio Vizzini <giorgiovizzini9@gmail.com>
2020-12-03 10:10:45 +0100

    First commit

---
+++ posts_csv.py
@@ -0,0 +1,85 @@
+import requests
+import json
+import csv
+import sys
+from collections import Counter
+import re
+import string
+
+def import_csv():
+    ht_list = []
+    with open('brexit.csv', 'r', encoding='utf-8') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+
+            words = row[2].replace("\n", " ")
+            words = words.replace("\xa0", " ")
+            words = words.replace("https:", "")
+            words = words.replace("http:", "")
+            words = words.replace("/", " ")
+            words = words.replace("|", " ")
+            words = words.replace("!", "")
+            words = words.replace("?", "")
+            words = words.replace("(", "")
+            words = words.replace(")", "")
+            words = words.replace(":", "")
+            words = words.replace("'", "")
+            words = words.replace("..", "")
+            words = words.replace("...", "")
+            words = words.replace("....", "")
+            words = words.replace(".", "")
+            words = words.replace("  ", " ")
+
+            words = words.split(" ")
+
+            for word in words:
+                try:
+                    if word[0] == "#":
+                        if "." in word[-1] or "," in word[-1] or "/" in word[-1] or "|" in word[-1]:
+                            word = word[:-1]
+
+                        if word.lower() not in ht_list:
+                            ht_list.append(word.lower())
+                except:
+                    pass
+
+        #with open('hashtag_list.csv', 'x', newline='') as hashtag_list:
+        #    wr = csv.writer(hashtag_list)
+        #    for row in ht_list:
+        #        wr.writerow([row])
+
+        print(ht_list)
+        print("The number of hashtags to analyze from the file is:", len(ht_list))
+        print(Counter(ht_list))
+        return ht_list
+
+import_csv()
+
+def hashtag_count():
+    ht_list = []
+    with open('hashtag_list.csv', 'r', encoding='utf-8') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            words = row[0]
+            words = words.split(" ")
+
+            for word in words:
+                try:
+                    if word[0] == "#":
+                        if "." in word[-1] or "," in word[-1] or "/" in word[-1] or "|" in word[-1] or ";" in word[-1]:
+                            word = word[:-1]
+                        if word.lower() not in ht_list:
+                            ht_list.append(word.lower())
+                except:
+                    pass
+
+        ht_list = ' '.join(ht_list)
+        ht_list_new = re.findall(r'\w+', ht_list)
+
+        print(ht_list_new)
+        print("The number of hashtags to analyze from the file is:", len(ht_list_new))
+        print(Counter(ht_list_new).most_common(6)[1:])
+        return ht_list_new
+
+import_csv()
+hashtag_count()

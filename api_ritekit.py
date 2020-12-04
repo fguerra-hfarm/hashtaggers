204b60b53600d1fffec08f7a1150684c59482a8d
Diego Vedova <diegovedova.stuff@gmail.com>
2020-12-04 11:38:51 +0100

    Working ritekit api

--- api_ritekit.py
+++ api_ritekit.py
@@ -1,10 +1,9 @@
 import requests
-import json
 from collections import Counter
 from tabulate import tabulate
-from twposts_csv import use_csv
+from posts_csv import use_csv
 
-# access_token = '456d04c7ad9ffba7a32a73fd4c7b9173a421bbaba76b'
+access_token = '456d04c7ad9ffba7a32a73fd4c7b9173a421bbaba76b'
 API_url = "https://api.ritekit.com/v1/stats/multiple-hashtags?tags="
 
 
@@ -12,8 +11,6 @@ def get_stats():
     # getting the top 5 hashtags by frequency, with their occurrence, from the analyzed .csv
     tags_list = ""
     c = Counter(use_csv()).most_common(5)
-    # for value, count in c:
-    #    print(value, count)
     # Fancy way to best show the most present hashtags to the user, for visual reference
     table = c
     print(tabulate(table, headers=["Hashtag - #", "Occurrence"], tablefmt="fancy_grid", numalign="center"))
@@ -25,11 +22,11 @@ def get_stats():
     payload = {}
     headers = {}
 
-    # response = requests.get(url, headers=headers, data=payload, params={'client_id': access_token})
-    # data = response.json()
-    # print(data)
+    response = requests.get(url, headers=headers, data=payload, params={'client_id': access_token})
+    data = response.json()
+    print(data)
 
-    # return data
+    return data
 
 
 get_stats()

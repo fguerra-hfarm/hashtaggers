2511d0ecd6140d2f7f5be006b51a0a7f4a1bdc34
Filippo Guerra <filippo.guerra@student.h-farm.com>
2020-12-10 23:07:16 +0100

    PEP-8 compliant

--- main.py
+++ main.py
@@ -11,75 +11,100 @@ from hashtagify_package import api_hashtagify
 
 
 def check_choice(choice):
+    """Menu choice check for positional arguments that must be 1 or 2 or 3"""
     input_choice = int(choice)
     if input_choice < 0 or input_choice > 4:
         raise argparse.ArgumentTypeError(
-            "\n\nNumber %s is an invalid choice, you must select between 1/2/3 to run the program!\n" % choice)
+            "\n\nNumber %s is an invalid choice, "
+            "you must select between 1/2/3 to run the program!\n" % choice)
     return input_choice
 
 
 def parse_arguments():
+    """Parsing the input for the program.
+
+    This function requires one positional argument which has three options
+    and offers three optional arguments for other functionalities such as
+    printing specific analytics or generating a new .csv file.
+    """
     # noinspection PyTypeChecker
     parser = argparse.ArgumentParser(
         formatter_class=argparse.RawDescriptionHelpFormatter,
         description=textwrap.dedent('''\
-                                                                              HASHTAGGERS ANALYTICS
-            ------------------------------------------------------------------------------------------------------------------------------------------------------------
+                           HASHTAGGERS ANALYTICS
+----------------------------------------------------------------------------
+    Service to extrapolate insights regarding hashtags used on Twitter!
+    Provided under paid license of the Hashtagify API for best accuracy
 
-                                                          Service to extrapolate insights regarding hashtags used on Twitter!
-                                                          Provided under paid license of the Hashtagify API for best accuracy
+   Choose between analysing any hashtag of your choice, the most frequent
+hashtags from a given dataset or go for the .csv cleaning and creation tool.
 
-                    Choose between analysing any hashtag of your choice, the most frequent hashtags from a given dataset or go for the .csv cleaning and creation tool.
-
-                    Menu:
-                    1 <- Hashtag of your choice
-                    2 <- Specific .csv dataset with a set of hashtags (upload yours in the '/use_csv_package/data' folder)
-                    3 <- Clean your dataset of posts and generate a .csv file of all the present hashtags inside
-                '''))
-    parser.add_argument('menu', type=check_choice, choices=[1, 2, 3], help='Menu selection choices',
+Menu:
+1 <- Hashtag of your choice
+2 <- Specific .csv dataset with a set of hashtags (see guidelines)
+3 <- Generate a .csv file of all the present hashtags inside you dataset
+'''))
+    parser.add_argument('menu', type=check_choice, choices=[1, 2, 3],
+                        help='Menu selection choices (pick one number)',
                         action='store')
-    parser.add_argument('-v', '--verbose', help='Complete hashtag information in a verbose format',
-                        action='store_true')
-    parser.add_argument('-t', '--tables', help='Specific tables of insights regarding the selected hashtag',
-                        action='store_true')
-    parser.add_argument('-c', '--correlated', help='Specific correlated hashtags to the one that has been selected',
-                        action='store_true')
+    parser.add_argument('-v', '--verbose',
+                        help='Complete hashtag information in a '
+                             'verbose format', action='store_true')
+    parser.add_argument('-t', '--tables',
+                        help='Specific tables of insights regarding '
+                             'the selected hashtag', action='store_true')
+    parser.add_argument('-c', '--correlated',
+                        help='Specific correlated hashtags to the one '
+                             'that has been selected', action='store_true')
     print()
     arguments = parser.parse_args()
     return arguments
 
 
 try:
+    # Token validity check
     api_token.check_token()
+    # Parsing the arguments
     args = parse_arguments()
+
+    # Hashtagify API use
+    # --------------------------------
+    # Simple chosen hashtag
     if args.menu == 1:
         input_ht = input("Hashtag that you want to analyze: #")
         if args.verbose:
-            api_hashtagify.get_insights(vot='v', hashtag=input_ht)
+            api_hashtagify.get_insights(vot='v', hashtag=input_ht.lower())
         elif args.tables:
-            api_hashtagify.get_insights(vot='t', hashtag=input_ht)
+            api_hashtagify.get_insights(vot='t', hashtag=input_ht.lower())
         elif args.correlated:
-            api_hashtagify.get_insights(vot='c', hashtag=input_ht)
+            api_hashtagify.get_insights(vot='c', hashtag=input_ht.lower())
         else:
-            api_hashtagify.get_insights(hashtag=input_ht)
+            api_hashtagify.get_insights(hashtag=input_ht.lower())
+    # --------------------------------
+    # Hashtag pick with reference to the given dataset "Most common" table
     elif args.menu == 2:
-        print()
-        table = Counter(posts_csv.use_post_csv(filename="dataset.csv").most_common(10))
-        print(tabulate(table, headers=["Hashtag - #", "Occurrence"], tablefmt="fancy_grid", numalign="center"))
+        topx = Counter(posts_csv.use_post_csv(
+            filename='dataset.csv'))
+        print(tabulate(topx.most_common(5),
+                       headers=['Hashtag - #', 'Occurrence'],
+                       tablefmt='fancy_grid', numalign='center'))
         sleep(0.5)
-        input_ht = input(f"\nFrom the given table, you may choose one of the most "
-                         f"recurrent hashtags for a complete analysis: #")
+        input_ht = input(f"\nFrom the given table, you may choose one of the "
+                         f"most recurrent hashtags for a complete analysis: #")
         if args.verbose:
-            api_hashtagify.get_insights(vot='v', hashtag=input_ht)
+            api_hashtagify.get_insights(vot='v', hashtag=input_ht.lower())
         elif args.tables:
-            api_hashtagify.get_insights(vot='t', hashtag=input_ht)
+            api_hashtagify.get_insights(vot='t', hashtag=input_ht.lower())
         elif args.correlated:
-            api_hashtagify.get_insights(vot='c', hashtag=input_ht)
+            api_hashtagify.get_insights(vot='c', hashtag=input_ht.lower())
         else:
-            api_hashtagify.get_insights(hashtag=input_ht)
+            api_hashtagify.get_insights(hashtag=input_ht.lower())
+    # --------------------------------
+    # Generation of a new .csv file of only hashtags
     elif args.menu == 3:
-        ht_csv.generate_csv(filename="dataset.csv")
+        ht_csv.generate_csv(filename='dataset.csv')
 
+# Possible occurring errors in using the program
 except ValueError:
     print("Something went wrong, please retry!")
     sys.exit()
--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 import os
 import setuptools
 
-with open(os.path.dirname(os.path.abspath(__file__)) + '/README.md', 'r') as ld:
+
+"""Hashtagger Analytics Package full details and requirements."""
+
+with open(os.path.dirname(os.path.abspath(__file__))
+          + '/README.md', 'r') as ld:
     long_description = ld.read()
 
 setuptools.setup(
     name='hashtagger_analytics',
     version='1.0',
-    description='Service to extrapolate insights regarding hashtags used on Twitter! '
-                'Provided under paid license of the Hashtagify API for best accuracy',
+    description='Service to extrapolate insights regarding hashtags '
+                'used on Twitter! ''Provided under paid license of the '
+                'Hashtagify API for best accuracy',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/fguerra-hfarm/hashtaggers',
@@ -21,9 +26,11 @@ setuptools.setup(
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3'],
-    keywords='hashtagify, analytics, twitter, hashtags, university, api, python',
+    keywords='hashtagify, analytics, twitter, hashtags, '
+             'university, api, python',
     packages=setuptools.find_packages(
-        include=['hashtagify_package', 'hashtagify_package.*', 'use_csv_package', 'use_csv_package.*']),
+        include=['hashtagify_package', 'hashtagify_package.*',
+                 'use_csv_package', 'use_csv_package.*']),
     python_requires='>=3',
     install_requires=['argparse', 'tabulate', 'requests', 'datapackage']
 )

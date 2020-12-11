c7dbc2628236a883527968cb9e5fc3c7467d6632
Giorgio Vizzini <giorgiovizzini9@gmail.com>
2020-12-11 11:55:39 +0100

    PEP-8 compliant

--- hashtagify_package/api_token.py
+++ hashtagify_package/api_token.py
@@ -4,9 +4,14 @@ import requests
 
 
 def get_access_token():
-    """Get the ACCESS_TOKEN for the API service"""
+    """Generate the ACCESS_TOKEN for the API service
 
-    endpoint = "https://api.hashtagify.me/oauth/token"
+    All the process is automated.
+    Just get your client_id and client_secret from your hashtagify
+    account settings to proceed.
+    """
+
+    endpoint = 'https://api.hashtagify.me/oauth/token'
 
     print("Generating your access token to use the API service")
     client_id = input("Enter your consumer key: ")
@@ -18,11 +23,13 @@ def get_access_token():
         'content-type': "application/x-www-form-urlencoded"
     }
 
-    response = requests.request("POST", endpoint, data=payload,
+    response = requests.request('POST', endpoint, data=payload,
                                 headers=headers)
-    access_token = json.loads(response.text)["access_token"]
+    access_token = json.loads(response.text)['access_token']
 
-    token_string = open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'api_token.txt'), 'w+')
+    # Writing a .txt file with the generated token for user reference
+    token_string = open(os.path.join(os.path.dirname(
+        os.path.abspath(__file__)), 'api_token.txt'), 'w+')
     token_string.write(f"This is your access token for the API service:"
                        f" {access_token}")
     print(f"\nYour access token for the API service is {access_token} and "
@@ -31,8 +38,10 @@ def get_access_token():
 
 
 def read_access_token():
+    """Use the ACCESS_TOKEN for the API service in the program functions"""
     access_token = ""
-    file = open(os.path.dirname(os.path.abspath(__file__)) + '/api_token.txt', 'r')
+    file = open(os.path.dirname(os.path.abspath(__file__)) +
+                '/api_token.txt', 'r')
     for line in file:
         access_token = line.split(' ')[-1]
     file.close()
@@ -40,7 +49,14 @@ def read_access_token():
 
 
 def check_token():
-    if os.path.isfile(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'api_token.txt')):
+    """Check the ACCESS_TOKEN validity
+
+    The token, if not valid, which might occur with error 401 of the API,
+    needs to be regenerated with the 'get_access_token' function. All the
+    process is automated. Just get your client_id and client_secret to proceed.
+    """
+    if os.path.isfile(os.path.join(os.path.dirname(
+            os.path.abspath(__file__)), 'api_token.txt')):
         endpoint = "https://api.hashtagify.me/1.0/tag/"
         headers = {
             'authorization': "Bearer " + read_access_token(),
@@ -55,6 +71,7 @@ def check_token():
             get_access_token()
             print("\nToken generated successfully!")
     else:
-        print("\nYou first need to generate your token in order to being able to use the API service!\n")
+        print("\nYou first need to generate your token in order to being"
+              " able to use the API service!\n")
         get_access_token()
         print("\nToken generated successfully!")
